31b80a19b5b1c429d4349f8d7a31918f535f77a2
Sean Fagan <seanfagan@eduroam-10-200-13-46.local.su.se>
2026-04-28 13:02:04 +0200

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,34 +5,7 @@ import FloodWarn from "./FloodWarn";
 const Header = () => {
   return (
     <header className="header">
-      <div className="header-image">
-
-        <a
-          href="https://uas.alaska.edu/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="/UAS.png"
-            alt="University of Alaska Southeast Logo"
-            className="logo"
-          />
-        </a>
-        <a href="https://nsf.gov/" target="_blank" rel="noopener noreferrer">
-          <img
-            src="/NSF.png"
-            alt="National Science Foundation Logo"
-            className="logo"
-          />
-        </a>
-                <a href="https://akcasc.org/" target="_blank" rel="noopener noreferrer">
-          <img
-            src="/ACASC2.png"
-            alt="Alaska Climate Adaptation Science Center Logo"
-            className="logo"
-          />
-        </a>
-      </div>
+      
       <div className="header-title">
         <h1>
           <a
